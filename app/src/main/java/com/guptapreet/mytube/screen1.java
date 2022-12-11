11f9c9a7128eb784bd36b0efb7fd1697476ea50e
guptapreet <115053359+guptapreet@users.noreply.github.com>
2022-12-11 12:54:33 +0530

    Initial commit

---
+++ app/src/main/java/com/guptapreet/mytube/screen1.java
@@ -0,0 +1,14 @@
+package com.guptapreet.mytube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class screen1 extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen1);
+    }
+}
